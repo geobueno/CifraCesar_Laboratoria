c9e474ecfa4503c9df5d2e4cf3daa7e7b48ee49a
Georgete Bueno <geobueno@gmail.com>
2018-06-07 19:08:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
-// Função para codificação da mensagem de texto inserida pelo usuário, com parâmetros de entrada da mensagem (message) e deslocamento de cifragem (lag).
+// FunÃ§Ã£o para codificaÃ§Ã£o da mensagem de texto inserida pelo usuÃ¡rio, com parÃ¢metros de entrada da mensagem (message) e deslocamento de cifragem (lag).
 function cipher(message,lag) {
 
-  // Condições para entrada de dados de maneira correta através do prompt. Verificação da existência de caracteres diferentes de letras e espaços e de valor nulo. 
+  // CondiÃ§Ãµes para entrada de dados de maneira correta atravÃ©s do prompt. VerificaÃ§Ã£o da existÃªncia de caracteres diferentes de letras e espaÃ§os e de valor nulo. 
   while (message.search(/[^A-Za-z\s]/) !== -1 || message[0] === undefined) {
-     var message = prompt("Digite uma mensagem para ser cifrada.\nObs.: A mensagem deve conter apenas letras(sem acentos) e espaços");
+    var message = prompt("Digite uma mensagem para ser cifrada.\nObs.: A mensagem deve conter apenas letras(sem acentos) e espaÃ§os");
   }
 
-  // Declaração de variáveis para processamento da mensagem recebida corretamente.
+  // DeclaraÃ§Ã£o de variÃ¡veis para processamento da mensagem recebida corretamente.
   var charCodeNum = 0;
   var letterCiphered = 0;
   var cipheredMessage = [];
 
-  // Verificação e processamento de codificação de cada um dos caracteres da mensagem de acordo com as condições características (letras minúsculas, maiúsculas e espaços).
+  // VerificaÃ§Ã£o e processamento de codificaÃ§Ã£o de cada um dos caracteres da mensagem de acordo com as condiÃ§Ãµes caracterÃ­sticas (letras minÃºsculas, maiÃºsculas e espaÃ§os).
   for (var i = 0; i < message.length; i++) {
-	charCodeNum = message[i].charCodeAt();
-	if (charCodeNum >= 65 && charCodeNum <= 90) {
-	  letterCiphered = (charCodeNum - 65 + lag) % 26 + 65;
+    charCodeNum = message[i].charCodeAt();
+    if (charCodeNum >= 65 && charCodeNum <= 90) {
+      letterCiphered = (charCodeNum - 65 + lag) % 26 + 65;
       cipheredMessage.push(String.fromCharCode(letterCiphered));
     } else if (charCodeNum >= 97 && charCodeNum <= 122) {
-	    letterCiphered = (charCodeNum - 97 + lag) % 26 + 97;
-	    cipheredMessage.push(String.fromCharCode(letterCiphered));
+        letterCiphered = (charCodeNum - 97 + lag) % 26 + 97;
+	cipheredMessage.push(String.fromCharCode(letterCiphered));
     } else if (charCodeNum === 32) {
-	    letterCiphered = 35;
-	    cipheredMessage.push(String.fromCharCode(letterCiphered));
+	letterCiphered = 35;
+	cipheredMessage.push(String.fromCharCode(letterCiphered));
 	}
   }
   // Valor final processado, caracteres reunidos para formar a mensagem criptografada enviada ao HTML
   return document.getElementById('messageCiphered').innerHTML = cipheredMessage.join('');
 }
-// Função para decodificação da mensagem de texto criptografada pela função anterior com parâmetros de entrada da mensagem codificada (message) e deslocamento de cifragem (lag).
+// FunÃ§Ã£o para decodificaÃ§Ã£o da mensagem de texto criptografada pela funÃ§Ã£o anterior com parÃ¢metros de entrada da mensagem codificada (message) e deslocamento de cifragem (lag).
 function decipher(message,lag) {
 
-  // Declaração de variáveis para processamento da mensagem criptografada.
+  // DeclaraÃ§Ã£o de variÃ¡veis para processamento da mensagem criptografada.
   var charCodeNum = 0;
   var letterDeciphered = 0;
   var decipheredMessage = [];
 
-  // Verificação e processamento de descodificação de cada um dos caracteres da mensagem de acordo com as condições características (letras minúsculas, maiúsculas e espaços).
+  // VerificaÃ§Ã£o e processamento de descodificaÃ§Ã£o de cada um dos caracteres da mensagem de acordo com as condiÃ§Ãµes caracterÃ­sticas (letras minÃºsculas, maiÃºsculas e espaÃ§os).
   for (var i = 0; i < message.length; i++) {
-	charCodeNum = message[i].charCodeAt();
-	if (charCodeNum >= 65 && charCodeNum <= 90) {
-	  letterDeciphered = (charCodeNum - 90 - lag) % 26 + 90;
-	  decipheredMessage.push(String.fromCharCode(letterDeciphered));
-	} else if (charCodeNum >= 97 && charCodeNum <= 122) {
-	    letterDeciphered = (charCodeNum -122 - lag) % 26 + 122;
-	    decipheredMessage.push(String.fromCharCode(letterDeciphered));
-	} else if (charCodeNum === 35){
-	    letterDeciphered = 32;
-	    decipheredMessage.push(String.fromCharCode(letterDeciphered));
-	}
+    charCodeNum = message[i].charCodeAt();
+    if (charCodeNum >= 65 && charCodeNum <= 90) {
+      letterDeciphered = (charCodeNum - 90 - lag) % 26 + 90;
+      decipheredMessage.push(String.fromCharCode(letterDeciphered));
+    } else if (charCodeNum >= 97 && charCodeNum <= 122) {
+        letterDeciphered = (charCodeNum -122 - lag) % 26 + 122;
+	decipheredMessage.push(String.fromCharCode(letterDeciphered));
+    } else if (charCodeNum === 35){
+	 letterDeciphered = 32;
+	 decipheredMessage.push(String.fromCharCode(letterDeciphered));
+      }
   }
   // Valor final processado, caracteres reunidos para formar a mensagem descriptografada enviada ao HTML
   return document.getElementById('messageDeciphered').innerHTML = decipheredMessage.join('');
 }
 
-// Chamada das funções para codificação e decodificaçao da mensagem de texto inserida pelo usuário, com parâmetros de entrada da mensagem (userMessage) e deslocamento de cifragem (lagFactor).
+// Chamada das funÃ§Ãµes para codificaÃ§Ã£o e decodificaÃ§ao da mensagem de texto inserida pelo usuÃ¡rio, com parÃ¢metros de entrada da mensagem (userMessage) e deslocamento de cifragem (lagFactor).
 var messageCiphered = cipher('0',33);
 var messageDeciphered = decipher(messageCiphered,33);
 
